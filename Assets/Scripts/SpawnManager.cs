92317c704ba3b643b5f1066751e2b59631d67a94
Blu_E_rash <163538035+Blu-E-Trash@users.noreply.github.com>
2024-06-23 16:05:00 +0900

    3.3finish
    3.3finish

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,15 +6,20 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private PlayerController playerControllerScript;
 
     private float startDelay = 2;
     private float repeaatRate = 2;
     void Start()
     {
-        InvokeRepeating("SpawnObstacle",startDelay,repeaatRate);
+        InvokeRepeating("SpawnObstacle", startDelay,repeaatRate);
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
     void SpawnObstacle()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        if (playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
     }
 }
